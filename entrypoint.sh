5cc84e5cff4645c39d08e05e442e30501936f30f
Brian Morrison II <brian@brianmorrison.me>
2023-02-15 13:52:28 -0500

    fix github output

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,11 @@ eval $command
 if [ "true" == "$7" ];then
   password_name="gha$( date +%s )"
   jout=$(pscale password create $1 $2 $password_name -f json)
+  username=$( jq -r '.name' <<< "${jout}" )
+  password=$( jq -r '.plain_text' <<< "${jout}" )
+  hostname=$( jq -r '.database_branch.access_host_url' <<< "${jout}" )
   echo "password_name=$password_name" >> $GITHUB_OUTPUT
-  echo "username=$( jq -r '.name' <<< "${jout}")" >> $GITHUB_OUTPUT
-  echo "password=$( jq -r '.plain_text' <<< "${jout}")" >> $GITHUB_OUTPUT
-  echo "hostname=$( jq -r '.database_branch.access_host_url' <<< "${jout}")" >> $GITHUB_OUTPUT
+  echo "username=$username" >> $GITHUB_OUTPUT
+  echo "password=$password" >> $GITHUB_OUTPUT
+  echo "hostname=$hostname" >> $GITHUB_OUTPUT
 fi
