6d47f4bb1401c0c460b21b256107cabadeedba15
Brian Morrison II <brian@brianmorrison.me>
2023-03-07 12:57:10 -0500

    consolidate actions

---
+++ deploy-deploy-request/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+command="pscale deploy-request deploy $1 $2 --org $3 \
+  --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME"
+
+
+
+cmdout=$(eval $command)
+
+if [ "true" == "$4" ];then
+  $statusout = 
+fi
+
+number=$(jq -n "$cmdout" | jq '.number')
+echo "number=$number" >> $GITHUB_OUTPUT
